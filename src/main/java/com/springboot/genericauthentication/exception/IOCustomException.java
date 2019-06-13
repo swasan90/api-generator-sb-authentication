a73ca7e862e1eedf2993c315b2e17d7a4bcecea1
unknown <swathytech@outlook.com>
2019-06-13 16:17:18 -0400

    created new custom exception to catch IO exception

---
+++ src/main/java/com/springboot/genericauthentication/exception/IOCustomException.java
@@ -0,0 +1,13 @@
+package com.springboot.genericauthentication.exception;
+
+public class IOCustomException extends Exception {
+
+	 
+	private static final long serialVersionUID = 1L;
+	
+	public IOCustomException(String message) {
+		super(message);
+	}
+
+
+}
