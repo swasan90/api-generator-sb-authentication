3e9d70c87d2a6e621192b458c0d51d385f0d7139
unknown <swathytech@outlook.com>
2019-06-13 16:13:16 -0400

    created email service impl class

--- src/main/java/com/springboot/genericauthentication/email/EmailServiceImpl.java
+++ src/main/java/com/springboot/genericauthentication/email/EmailServiceImpl.java
@@ -1,12 +1,40 @@
 /**
- * 
+ * Class to implement Email Service
  */
 package com.springboot.genericauthentication.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.springboot.genericauthentication.exception.MailErrorException;
 
 /**
  * @author swathy
  *
  */
-public class EmailServiceImpl {
+@Service("emailService")
+public class EmailServiceImpl implements EmailService {
+	
+	@Autowired
+	public JavaMailSender emailSender;
+
+	@Override
+	public void sendEmailMessage(String to, String subject, String text) throws MailErrorException {
+		 try {
+			 SimpleMailMessage message = new SimpleMailMessage();
+			 message.setTo(to);
+			 message.setSubject(subject);
+			 message.setText(text);
+			 
+			 emailSender.send(message);
+		 }catch(MailException exception) {
+			 throw new MailErrorException(exception.getMessage());
+		 }		
+	}
+	
+	
 
 }
