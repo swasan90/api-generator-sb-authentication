d01d60005320891a1deed1abbfe247f64cb18548
unknown <swathytech@outlook.com>
2019-06-13 13:02:16 -0400

    added java mail sender bean in the classpath

--- src/main/java/com/springboot/genericauthentication/GenericAuthenticationApplication.java
+++ src/main/java/com/springboot/genericauthentication/GenericAuthenticationApplication.java
@@ -1,8 +1,12 @@
 package com.springboot.genericauthentication;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
@@ -20,6 +24,27 @@ public class GenericAuthenticationApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		
+		//Credentials
+		mailSender.setUsername("technojuggernaut@gmail.com");
+		mailSender.setPassword("asd123qwe456");
+		
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol","smtp");
+		props.put("mail.smtp.auth","true");
+		props.put("mail.smtp.starttls.enable","true");
+		props.put("mail.debug","true");
+		
+		
+		return mailSender;
 	}
 	
 }
