f2ddfd850e3e69be752eb65d9631591800b2ab74
unknown <swathytech@outlook.com>
2019-06-13 16:12:44 -0400

    created interface to implement email service methods

--- src/main/java/com/springboot/genericauthentication/email/EmailService.java
+++ src/main/java/com/springboot/genericauthentication/email/EmailService.java
@@ -3,11 +3,13 @@
  */
 package com.springboot.genericauthentication.email;
 
+import com.springboot.genericauthentication.exception.MailErrorException;
+
 /**
  * @author swathy
  *
  */
 public interface EmailService {
 
-	void sendEmailMessage(String to,String subject,String text );
+	void sendEmailMessage(String to,String subject,String text ) throws MailErrorException;
 }
