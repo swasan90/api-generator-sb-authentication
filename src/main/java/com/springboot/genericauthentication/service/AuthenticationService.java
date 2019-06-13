e5a934bd5ca47e81a2aff957627e1a3dcf6f66d2
unknown <swathytech@outlook.com>
2019-06-13 16:15:03 -0400

    added mailerror exception in throw declaration

--- src/main/java/com/springboot/genericauthentication/service/AuthenticationService.java
+++ src/main/java/com/springboot/genericauthentication/service/AuthenticationService.java
@@ -4,6 +4,7 @@
 package com.springboot.genericauthentication.service;
 
 import com.springboot.genericauthentication.exception.EntityFoundException;
+import com.springboot.genericauthentication.exception.MailErrorException;
 import com.springboot.genericauthentication.models.User;
 
 /**
@@ -12,5 +13,5 @@ import com.springboot.genericauthentication.models.User;
  */
 public interface AuthenticationService {
 	
-	boolean registerUser(User user) throws EntityFoundException;
+	boolean registerUser(User user) throws EntityFoundException,MailErrorException;
 }
