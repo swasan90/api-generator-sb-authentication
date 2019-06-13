fcda67e8b9940c85a76a20f866c793f74ca082e6
unknown <swathytech@outlook.com>
2019-06-13 16:11:44 -0400

    created mail object pojo for sending emails

---
+++ src/main/java/com/springboot/genericauthentication/models/MailObject.java
@@ -0,0 +1,34 @@
+/**
+ * Java POJO class to define the mail object
+ */
+package com.springboot.genericauthentication.models;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+/**
+ * @author swathy
+ *
+ */
+ @Data
+public class MailObject {
+	
+	private String subject;
+	
+	private String message;
+	
+	@Email
+	@NotBlank(message="Email id is mandatory")	
+	private String to;
+	
+	public MailObject() {};
+	
+	public MailObject(String to,String subject,String message) {
+		this.to =to;
+		this.subject = subject;
+		this.message = message;
+	}
+
+}
