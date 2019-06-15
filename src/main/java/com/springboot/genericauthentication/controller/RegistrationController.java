3b91b91885a22e7c2d39b09f86464c32ca10976f
unknown <swathytech@outlook.com>
2019-06-14 20:37:58 -0400

    created a get mapping function to activate the user account

--- src/main/java/com/springboot/genericauthentication/controller/RegistrationController.java
+++ src/main/java/com/springboot/genericauthentication/controller/RegistrationController.java
@@ -12,16 +12,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.genericauthentication.exception.EntityFoundException;
-import com.springboot.genericauthentication.exception.IOCustomException;
 import com.springboot.genericauthentication.exception.MailErrorException;
 import com.springboot.genericauthentication.models.ResponseMessage;
 import com.springboot.genericauthentication.models.User;
 import com.springboot.genericauthentication.service.AuthenticationService;
+import com.springboot.genericauthentication.token.service.TokenService;
 
 /**
  * @author swathy
@@ -34,18 +36,49 @@ public class RegistrationController {
 	
 	@Autowired
 	private AuthenticationService authService;
-	 
 	
+	@Autowired
+	private TokenService tokenService;
+	
+	private ResponseMessage res;
+	
+	public RegistrationController() {
+		this.res = new ResponseMessage();
+	}
+	 
+	/**
+	 * Function to register a user
+	 * @param user
+	 * @return
+	 * @throws EntityFoundException
+	 * @throws MailErrorException
+	 * @throws IOException
+	 */
 	@PostMapping(value="/register",consumes="application/json",produces="application/json")
-	public ResponseEntity<ResponseMessage> registerAccount(@Valid @RequestBody User user) throws EntityFoundException, MailErrorException,IOException{		
-		ResponseMessage res = new ResponseMessage();
+	public ResponseEntity<ResponseMessage> registerAccount(@Valid @RequestBody User user) throws EntityFoundException, MailErrorException,IOException{		 
 		if( authService.registerUser(user)) {	
 			logger.info("Successfully created user account");
-			return new ResponseEntity<ResponseMessage>( res.setMessage("Successfully created account",HttpStatus.CREATED),HttpStatus.CREATED); 
+			return new ResponseEntity<ResponseMessage>( this.res.setMessage("Successfully created account",HttpStatus.CREATED),HttpStatus.CREATED); 
 		}else {
 			logger.info("Unable to create user account");
-			return new ResponseEntity<ResponseMessage>( res.setMessage("Email id exist.Cannot creat account",HttpStatus.BAD_REQUEST),HttpStatus.BAD_REQUEST); 
+			return new ResponseEntity<ResponseMessage>( this.res.setMessage("Email id exist.Cannot creat account",HttpStatus.BAD_REQUEST),HttpStatus.BAD_REQUEST); 
 		}  
+	}
+	
+	/**
+	 * Function to activate account upon clicking the activation link
+	 * @param token
+	 * @return
+	 */
+	@GetMapping("/")
+	public ResponseEntity<ResponseMessage> activateAccount(@Valid @RequestParam(name="token") String token){
+		if(tokenService.validateToken(token)) {
+			logger.info("Successfully validated token");
+			return new ResponseEntity<ResponseMessage>(this.res.setMessage("Your account has been activated.Please login with your credentials",HttpStatus.OK),HttpStatus.OK);
+		}else {
+			logger.info("Unable to activate user account" );
+			return new ResponseEntity<ResponseMessage>(this.res.setMessage("Your token was expired.Kindly register again",HttpStatus.UNAUTHORIZED),HttpStatus.UNAUTHORIZED);
+		}
 	}
 
 }
