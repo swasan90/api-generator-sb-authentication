cca79b0e58fbcb4da626767cf31e3121dd45b998
unknown <swathytech@outlook.com>
2019-06-17 10:48:32 -0400

    created jwt authentication filter class to authenticate user's credentials

---
+++ src/main/java/com/springboot/genericauthentication/jwt/SecurityConstants.java
@@ -0,0 +1,18 @@
+/**
+ * Class to define the security constants variables
+ */
+package com.springboot.genericauthentication.jwt;
+
+/**
+ * @author swathy
+ *
+ */
+public class SecurityConstants {
+	
+	public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final long EXPIRATION_TIME = 216_000_000; // 1 day
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/register";
+
+}
