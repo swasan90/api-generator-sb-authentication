0c6f44377aa0c3d117f5bef47bf10f051fb935ac
unknown <swathytech@outlook.com>
2019-06-17 14:20:12 -0400

    modified the user class to authuser

--- src/main/java/com/springboot/genericauthentication/repository/AuthenticationRepository.java
+++ src/main/java/com/springboot/genericauthentication/repository/AuthenticationRepository.java
@@ -6,16 +6,16 @@ package com.springboot.genericauthentication.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.springboot.genericauthentication.models.User;
+import com.springboot.genericauthentication.models.AuthUser;
 
 /**
  * @author swathy
  *
  */
 @Repository
-public interface AuthenticationRepository extends JpaRepository<User,Long> {
+public interface AuthenticationRepository extends JpaRepository<AuthUser,Long> {
 	
-	User findByEmail(String email);
+	AuthUser findByEmail(String email);
 	
-	User findById(long id);
+	AuthUser findById(long id);
 }
