f5e30cc73a9a3afdd9c4e235de5cb4e469ff0d8a
unknown <swathytech@outlook.com>
2019-06-17 11:20:16 -0400

    created security constants file to define constant for jwt auth

--- src/main/java/com/springboot/genericauthentication/jwt/SecurityConstants.java
+++ src/main/java/com/springboot/genericauthentication/jwt/SecurityConstants.java
@@ -9,7 +9,7 @@ package com.springboot.genericauthentication.jwt;
  */
 public class SecurityConstants {
 	
-	public static final String SECRET = "SecretKeyToGenJWTs";
+	public static final String SECRET = "AuthSecretKey";
     public static final long EXPIRATION_TIME = 216_000_000; // 1 day
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
