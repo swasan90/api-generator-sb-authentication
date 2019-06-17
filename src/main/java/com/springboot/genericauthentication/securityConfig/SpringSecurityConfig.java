cd51d27b2ce4ff1ddd154b03899b5ad2641f21ae
unknown <swathytech@outlook.com>
2019-06-17 14:21:14 -0400

    added jwt filter in configure method

--- src/main/java/com/springboot/genericauthentication/securityConfig/SpringSecurityConfig.java
+++ src/main/java/com/springboot/genericauthentication/securityConfig/SpringSecurityConfig.java
@@ -3,12 +3,24 @@
  */
 package com.springboot.genericauthentication.securityConfig;
 
+import static com.springboot.genericauthentication.jwt.SecurityConstants.SIGN_UP_URL;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.springboot.genericauthentication.jwt.JWTAuthenticationFilter;
+import com.springboot.genericauthentication.jwt.JWTAuthorizationFilter;
+import com.springboot.genericauthentication.user.service.UserDetailsServiceImpl;
 
 /**
  * @author swathy
@@ -18,6 +30,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	private UserDetailsServiceImpl userDetailsService;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public SpringSecurityConfig(UserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder ) {
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+		this.userDetailsService = userDetailsService;
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
     {
@@ -25,17 +50,34 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 			.cors()
 			.and()
 			.csrf().disable()
-			.authorizeRequests().antMatchers(HttpMethod.POST,"/login").permitAll()
-			.antMatchers(HttpMethod.POST,"/register").permitAll()
-			.antMatchers(HttpMethod.GET,"/register").permitAll()
-			.antMatchers(HttpMethod.GET,"/").permitAll()
+			.authorizeRequests()
+			.antMatchers(HttpMethod.GET,"/").permitAll()			 
+			.antMatchers(HttpMethod.POST,SIGN_UP_URL).permitAll()
+			.antMatchers(HttpMethod.GET,SIGN_UP_URL).permitAll()			
 			.anyRequest().authenticated().and()
-			//.addFilter(new JWTAuthenticationFilter(authenticationManager()))
-	        //.addFilter(new JWTAuthorizationFilter(authenticationManager()))
+			.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+	        .addFilter(new JWTAuthorizationFilter(authenticationManager()))
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 		http.headers().frameOptions().disable();	
 		
     }
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+		
+	}
 
 }
