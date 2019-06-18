4e633fcabc84f96b53eb9782fdf2f280270efb35
unknown <swathytech@outlook.com>
2019-06-18 15:37:38 -0400

    removed jsonmanaged reference annotation

--- src/main/java/com/springboot/genericauthentication/models/UserToken.java
+++ src/main/java/com/springboot/genericauthentication/models/UserToken.java
@@ -13,8 +13,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import lombok.Data;
 
 /**
