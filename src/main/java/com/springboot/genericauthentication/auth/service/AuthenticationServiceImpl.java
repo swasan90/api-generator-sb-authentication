2b2b13058172f085feb3022f15b994b114d1257b
unknown <swathytech@outlook.com>
2019-06-18 15:29:22 -0400

    removed token methods to token service and refactored the code in register user

--- src/main/java/com/springboot/genericauthentication/auth/service/AuthenticationServiceImpl.java
+++ src/main/java/com/springboot/genericauthentication/auth/service/AuthenticationServiceImpl.java
@@ -4,13 +4,7 @@
 package com.springboot.genericauthentication.auth.service;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Date;
-import java.util.UUID;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +13,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.sendgrid.Content;
 import com.sendgrid.Email;
 import com.sendgrid.Mail;
 import com.sendgrid.Request;
@@ -30,9 +21,8 @@ import com.springboot.genericauthentication.email.EmailService;
 import com.springboot.genericauthentication.exception.EntityFoundException;
 import com.springboot.genericauthentication.exception.MailErrorException;
 import com.springboot.genericauthentication.models.AuthUser;
-import com.springboot.genericauthentication.models.UserToken;
-import com.springboot.genericauthentication.repository.AuthenticationRepository;
-import com.springboot.genericauthentication.repository.UserTokenRepository;
+import com.springboot.genericauthentication.repository.AuthenticationRepository; 
+import com.springboot.genericauthentication.token.service.TokenService;
 
 /**
  * @author swathy
@@ -47,10 +37,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 	private AuthenticationRepository authRepo;
 
 	@Autowired
-	private UserTokenRepository userTokenRepo;
+	private EmailService emailService;
 
 	@Autowired
-	private EmailService emailService;
+	private TokenService tokenService;
 
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
@@ -86,14 +76,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 				logger.info("Created User account in database");
 
 				// Generating token for the new user
-				URI tokenUrl = generateTokenForNewUser(newUser);
+				URI tokenUrl = tokenService.constructTokenUrl(tokenService.generateTokenForUser(newUser), "activate");
 				logger.info("Generated token");
 
 				// Sending email to the new user with the token for more authentication
-				Mail mailObj = constructMailBody(newUser, tokenUrl);
-				Request request = new Request();
-				SendGrid sg = new SendGrid(apiKey);
-				emailService.sendEmailMessage(mailObj, request, sg);
+				Mail mailObj = tokenService.constructMailBody(newUser, tokenUrl);
+				emailService.sendEmailMessage(mailObj, new Request(), new SendGrid(apiKey));
 				logger.info("Mail sent");
 
 				return true;
@@ -106,49 +94,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 			throw new EntityFoundException(e.getMessage());
 		}
 
-	}
-
-	/**
-	 * Function to generate token for the user
-	 * 
-	 * @param user
-	 * @return String
-	 */
-	private URI generateTokenForNewUser(AuthUser user) {
-		String token = String.valueOf(UUID.randomUUID()).replace("-", "");
-		Instant expirationDate = Instant.now().plus(Duration.ofHours(24));
-		UserToken userToken = new UserToken(token, user, expirationDate);
-		userTokenRepo.save(userToken);
-		return constructTokenUrl(token);
-	}
-
-	/**
-	 * Function to construct url with token for the new user
-	 * 
-	 * @param token
-	 * @return String
-	 * @throws MalformedURLException
-	 */
-	private URI constructTokenUrl(String token) {
-		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().queryParam("token", token).build().toUri();
-		return uri;
-	}
-
-	/**
-	 * Function to construct mail object
-	 * 
-	 * @param user
-	 * @param tokenUrl
-	 * @return Mail
-	 */
-	private Mail constructMailBody(AuthUser user, URI tokenUrl) {
-		String subject = "Activate your account on your registration";
-		String body = " Welcome " + user.getFirstName()
-				+ ",<br/><p>You are receiving this email because you have registered in our site.\n\n Please click on the below link to activate your account.<br/>"
-				+ tokenUrl
-				+ "<p>Kindly note that this link will be activated only for 24 hours from now.<br/><br/><br/>Regards,<br/> Admin Team</p>";
-		Content content = new Content("text/html", body);
-		return new Mail(from, subject, new Email(user.getEmail()), content);
 	}
 
 }
