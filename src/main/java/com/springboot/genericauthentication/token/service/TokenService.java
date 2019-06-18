e84c3bf16b2b420a4f0fb3a077684d66971b709b
unknown <swathytech@outlook.com>
2019-06-18 15:36:34 -0400

    implemented token service methods

--- src/main/java/com/springboot/genericauthentication/token/service/TokenService.java
+++ src/main/java/com/springboot/genericauthentication/token/service/TokenService.java
@@ -3,13 +3,32 @@
  */
 package com.springboot.genericauthentication.token.service;
 
+import java.net.URI;
+
+ 
+import com.sendgrid.Mail;
+ 
+import com.springboot.genericauthentication.models.AuthUser;
+
 /**
  * @author swathy
  *
  */
 public interface TokenService {
+	 
 	
 	//Function definition to validate user's token
 	boolean validateToken(String token);
+	
+	//Function to create token for the user
+	String generateTokenForUser(AuthUser user);
+	
+	//Function to construct URL with token for the user
+	URI constructTokenUrl(String token,String action);
+	
+	//Function to construct message for sending email
+	Mail constructMailBody(AuthUser user,URI url);
+	
+	 
 
 }
--- src/main/java/com/springboot/genericauthentication/token/service/TokenServiceImpl.java
+++ src/main/java/com/springboot/genericauthentication/token/service/TokenServiceImpl.java
@@ -3,12 +3,21 @@
  */
 package com.springboot.genericauthentication.token.service;
 
+import java.net.URI;
+import java.time.Duration;
 import java.time.Instant;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import com.sendgrid.Mail;
 import com.springboot.genericauthentication.models.AuthUser;
+import com.springboot.genericauthentication.models.MailObject;
 import com.springboot.genericauthentication.models.UserToken;
 import com.springboot.genericauthentication.repository.AuthenticationRepository;
 import com.springboot.genericauthentication.repository.UserTokenRepository;
@@ -25,7 +34,13 @@ public class TokenServiceImpl implements TokenService {
 
 	@Autowired
 	private AuthenticationRepository authRepo;
-	
+
+	@Value("${sendgrid.api.key}")
+	private String apiKey;
+
+	@Value("${sendgrid.from.mail}")
+	private Email from;
+
 	/***
 	 * Function to validate user's token.
 	 */
@@ -48,17 +63,97 @@ public class TokenServiceImpl implements TokenService {
 		if (userToken != null) {
 			Instant current = Instant.now();
 			if (userToken.getExpirationDate().isAfter(current)) {
-				AuthUser user = authRepo.findById(userToken.getUser().getId());
-				if (user != null) {
-					user.setEnabled(true);
-					user.setStatus(true);
-					authRepo.save(user);
-					return true;
-				}
+				AuthUser user = authRepo.findById(userToken.getUser().getId());			 
+				if (user != null && !user.isEnabled()) {					 
+					activateUser(user);					
+				}	
+				return true;
 			}
 		}
 		return false;
 
+	}
+
+	/**
+	 * Function to activate the user upon registration
+	 * 
+	 * @param user
+	 */
+	private void activateUser(AuthUser user) {
+		user.setEnabled(true);
+		user.setStatus(true);
+		authRepo.save(user);
+	}
+
+	/**
+	 * Function to reset the user's password
+	 * 
+	 * @pa
+	 */
+
+	/**
+	 * Function to generate token for the user
+	 * 
+	 * @param user
+	 * @return String
+	 */
+	@Override
+	public String generateTokenForUser(AuthUser user) {
+		String token = String.valueOf(UUID.randomUUID()).replace("-", "");
+		Instant expirationDate = Instant.now().plus(Duration.ofHours(24));
+		UserToken userToken = new UserToken(token, user, expirationDate);
+		userTokenRepo.save(userToken);
+		return token;
+	}
+
+	/**
+	 * Function to construct url with token for the new user
+	 * 
+	 * @param token
+	 * @return URI
+	 */
+	@Override
+	public URI constructTokenUrl(String token, String action) {
+		URI uri = null;
+		if (action.equals("activate")) {
+			uri = ServletUriComponentsBuilder.fromCurrentContextPath().path(action).queryParam("token", token).build()
+					.toUri();
+		} else if (action.equals("reset")) {
+			uri = ServletUriComponentsBuilder.fromCurrentContextPath().path(action).queryParam("token", token).build()
+					.toUri();
+		}
+		return uri;
+	}
+
+	/**
+	 * Function to construct mail object
+	 * 
+	 * @param user
+	 * @param tokenUrl
+	 * @return Mail
+	 */
+	@Override
+	public Mail constructMailBody(AuthUser user, URI tokenUrl) {
+		MailObject mailObj = new MailObject();
+		if (user.isEnabled()) {
+			mailObj.setSubject("Did you forgot your password?");
+			mailObj.setGreeting("Welcome " + user.getFirstName());
+			mailObj.setMessage(
+					",<br/><p>You are receiving this email because you have clicked forgot password.\n\n Please click on the below link to reset your password.<br/>"
+							+ tokenUrl
+							+ "<p>Kindly note that this link will be activated only for 24 hours from now.<br/><br/><br/>Regards,<br/> Admin Team</p>");
+
+		} else {
+			System.out.println("printing token "+tokenUrl);
+			mailObj.setSubject("Activate your account on your registration");
+			mailObj.setGreeting("Welcome " + user.getFirstName());
+			mailObj.setMessage(
+					",<br/><p>You are receiving this email because you have registered in our site.\n\n Please click on the below link to activate your account.<br/>"
+							+ tokenUrl
+							+ "<p>Kindly note that this link will be activated only for 24 hours from now.<br/><br/><br/>Regards,<br/> Admin Team</p>");
+		}
+		Content content = new Content("text/html", mailObj.getGreeting() + mailObj.getMessage());
+		return new Mail(from, mailObj.getSubject(), new Email(user.getEmail()), content);
 	}
 
 }
