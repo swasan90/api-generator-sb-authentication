d741369665aa005acab909f355fa3d95898de8b8
unknown <swathytech@outlook.com>
2019-06-18 15:33:08 -0400

    added security constants

--- src/main/java/com/springboot/genericauthentication/jwt/SecurityConstants.java
+++ src/main/java/com/springboot/genericauthentication/jwt/SecurityConstants.java
@@ -14,5 +14,8 @@ public class SecurityConstants {
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/register";
-
+	public static final String FORGOT_PASSWORD_URL ="/forgotPassword";
+	public static final String RESET_PASSWORD_URL_PATH ="/reset";
+	public static final String ACTIVATE_USER ="/activate";
+	
 }
