0fb47492e555552a0c98a1cdbf1180d502dd683c
unknown <swathytech@outlook.com>
2019-06-18 15:31:48 -0400

    added security constants to handle http request

--- src/main/java/com/springboot/genericauthentication/jwt/JWTAuthenticationFilter.java
+++ src/main/java/com/springboot/genericauthentication/jwt/JWTAuthenticationFilter.java
@@ -3,6 +3,12 @@
  */
 package com.springboot.genericauthentication.jwt;
 
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static com.springboot.genericauthentication.jwt.SecurityConstants.EXPIRATION_TIME;
+import static com.springboot.genericauthentication.jwt.SecurityConstants.HEADER_STRING;
+import static com.springboot.genericauthentication.jwt.SecurityConstants.SECRET;
+import static com.springboot.genericauthentication.jwt.SecurityConstants.TOKEN_PREFIX;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -18,20 +24,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import com.auth0.jwt.JWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.genericauthentication.models.AuthUser;
-import com.auth0.jwt.JWT;
-import static com.springboot.genericauthentication.jwt.SecurityConstants.EXPIRATION_TIME;
-import static com.springboot.genericauthentication.jwt.SecurityConstants.SECRET;
-import static com.springboot.genericauthentication.jwt.SecurityConstants.HEADER_STRING;
-import static com.springboot.genericauthentication.jwt.SecurityConstants.TOKEN_PREFIX;
-import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
 /**
  * @author swathy
  *
  */
+
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
 	private AuthenticationManager authenticationManager;
@@ -39,9 +40,10 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	public JWTAuthenticationFilter(AuthenticationManager authManager) {
 		this.authenticationManager = authManager;
 	}
-	
+
 	/**
-	 * Function to  attempt the authentication and returns the authenticated object.
+	 * Function to attempt the authentication and returns the authenticated object.
+	 * 
 	 * @param HttpServletRequest, HttpServletResponse
 	 * @return authentication
 	 */
@@ -54,14 +56,17 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 			return authenticationManager.authenticate(
 					new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 
 	}
-	
+
 	/**
-	 * Function to create token for the user upon successful authentication and adds the token in response header along with token prefix.
+	 * Function to create token for the user upon successful authentication and adds
+	 * the token in response header along with token prefix.
+	 * 
 	 * @param HttpServletRequest, HttpServletResponse,FilterChain,Authentication
 	 * 
 	 */
