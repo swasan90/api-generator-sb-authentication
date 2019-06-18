27f8246a232d5114fa1b56197aa2f21eb1baa121
unknown <swathytech@outlook.com>
2019-06-18 15:38:05 -0400

    added rules in http configure method to allow/disallow request

--- src/main/java/com/springboot/genericauthentication/securityConfig/SpringSecurityConfig.java
+++ src/main/java/com/springboot/genericauthentication/securityConfig/SpringSecurityConfig.java
@@ -4,7 +4,10 @@
 package com.springboot.genericauthentication.securityConfig;
 
 import static com.springboot.genericauthentication.jwt.SecurityConstants.SIGN_UP_URL;
-
+import static com.springboot.genericauthentication.jwt.SecurityConstants.FORGOT_PASSWORD_URL;
+import static com.springboot.genericauthentication.jwt.SecurityConstants.RESET_PASSWORD_URL_PATH;
+import static com.springboot.genericauthentication.jwt.SecurityConstants.ACTIVATE_USER;
+ 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,13 +16,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; 
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import com.springboot.genericauthentication.jwt.JWTAuthenticationFilter;
-import com.springboot.genericauthentication.jwt.JWTAuthorizationFilter;
+import com.springboot.genericauthentication.jwt.JWTAuthorizationFilter; 
 import com.springboot.genericauthentication.user.service.UserDetailsServiceImpl;
 
 /**
@@ -37,22 +40,26 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 			BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 		this.userDetailsService = userDetailsService;
+
 	}
-	
-	
+
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
 	}
-	 
+
 	/**
-	 * Function to implement the rules to allow the http url's. 
+	 * Function to implement the rules to allow the http url's.
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/").permitAll()
-				.antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll().antMatchers(HttpMethod.GET, SIGN_UP_URL)
-				.permitAll().anyRequest().authenticated().and()
+				.antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+				.antMatchers(HttpMethod.GET, ACTIVATE_USER).permitAll()
+				.antMatchers(HttpMethod.POST,FORGOT_PASSWORD_URL).permitAll()
+				.antMatchers(HttpMethod.GET,RESET_PASSWORD_URL_PATH).permitAll()
+				.antMatchers(HttpMethod.POST,RESET_PASSWORD_URL_PATH).permitAll()
+				.anyRequest().authenticated().and()
 				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
 				.addFilter(new JWTAuthorizationFilter(authenticationManager())).sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -63,6 +70,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	/**
 	 * Function to define the cors filter setting.
+	 * 
 	 * @return
 	 */
 	@Bean
