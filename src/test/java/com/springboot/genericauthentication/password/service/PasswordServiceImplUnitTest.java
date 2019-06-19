b77e605196f1b84e2ad44ef3f7d6e4d3ddf2bb31
unknown <swathytech@outlook.com>
2019-06-19 00:12:28 -0400

    completed unit testing for password reset and forgot password

---
+++ src/test/java/com/springboot/genericauthentication/password/service/PasswordServiceImplUnitTest.java
@@ -0,0 +1,108 @@
+package com.springboot.genericauthentication.password.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.springboot.genericauthentication.email.EmailService;
+import com.springboot.genericauthentication.exception.MailErrorException;
+import com.springboot.genericauthentication.models.AuthUser;
+import com.springboot.genericauthentication.repository.AuthenticationRepository;
+import com.springboot.genericauthentication.token.service.TokenService;
+
+@ExtendWith(MockitoExtension.class)
+class PasswordServiceImplUnitTest {
+
+	@Mock
+	private AuthenticationRepository authRepo;
+	
+	private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+	
+	@Mock
+	private TokenService tokenService;
+	
+	@Mock
+	private EmailService emailService;
+	
+	AuthUser user;
+	
+	@InjectMocks
+	private PasswordService passwordService = new PasswordServiceImpl(this.bCryptPasswordEncoder);
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		user = new AuthUser("jim.carry@example.com",this.bCryptPasswordEncoder.encode("password"));
+		user.setConfirmPassword(user.getPassword());
+		user.setEnabled(true);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		user = null;
+	}
+	/**
+	 * Function to test forgot password
+	 * @throws MailErrorException
+	 * @throws IOException
+	 */
+	@DisplayName("Testing forgot password that returns true")
+	@Test
+	void testForgotPassword() throws MailErrorException, IOException {		 
+		 Mockito.when(authRepo.findByEmail(user.getEmail())).thenReturn(user);		 
+		 boolean expected = passwordService.forgotPassword(user.getEmail());
+		 assertEquals(expected,true);
+	}
+	
+	/**
+	 * Function to test forgot password that fails.
+	 * @throws MailErrorException
+	 * @throws IOException
+	 */
+	@DisplayName("Testing forgot password that returns false")
+	@Test
+	void testForgotPasswordReturnsFalse() throws MailErrorException, IOException {		
+		 Mockito.when(authRepo.findByEmail(user.getEmail())).thenReturn(null);		 
+		 boolean expected = passwordService.forgotPassword(user.getEmail());
+		 assertEquals(expected,false);
+	}
+	
+	
+	/**
+	 * Function to test reset password
+	 * @throws IOException
+	 */
+	@DisplayName("Test function to reset password")
+	@Test
+	void testResetPassword() throws IOException {
+		Mockito.when(authRepo.findByEmail(user.getEmail())).thenReturn(user);
+		boolean expected = passwordService.resetPassword(user);		
+		assertEquals(expected,true);
+		assertNotEquals("password",user.getPassword());
+		assertEquals(user.getPassword(),user.getConfirmPassword());
+	}
+	
+	/**
+	 * Function to test reset password that returns false
+	 * @throws IOException
+	 */
+	@DisplayName("Test function to reset password returns false")
+	@Test
+	public void testResetPasswordReturnsFalse() throws IOException {
+		Mockito.when(authRepo.findByEmail(user.getEmail())).thenReturn(null);
+		boolean expected = passwordService.resetPassword(user);
+		assertEquals(expected,false);
+	}
+
+}
