6552120f8fafca579ba86a12918f0bc8c206b2ed
unknown <swathytech@outlook.com>
2019-06-19 15:38:35 -0400

    completed integration test for registration controller

---
+++ src/test/java/com/springboot/genericauthentication/controller/RegistrationControllerIntegrationTest.java
@@ -0,0 +1,122 @@
+package com.springboot.genericauthentication.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.genericauthentication.models.AuthUser;
+
+@ExtendWith(MockitoExtension.class) 
+@WebAppConfiguration
+@SpringBootTest 
+@ContextConfiguration  
+class RegistrationControllerIntegrationTest {
+	
+	
+	@Autowired
+	private WebApplicationContext wac;
+	
+	private MockMvc mockMvc;
+	
+	AuthUser user;
+	
+	 
+
+
+	@BeforeEach
+	void setUp() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+		this.user = new AuthUser("Jim","Carry","jim.carry@example.com","password");
+		 
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		this.mockMvc = null;
+		this.user = null;
+		 
+	}
+	
+	/**
+	 * Function to test account registration
+	 * @throws Exception
+	 */
+	@DisplayName("Testing Register Account")
+	@Test
+	public void testRegisterAccount() throws Exception {
+		this.mockMvc.perform(post("/register").contentType("application/json").content(new ObjectMapper().writeValueAsString(this.user)))
+					.andDo(print())
+					.andExpect(status().isCreated())
+					.andExpect(jsonPath("$.message").value("Successfully created account"))
+					.andExpect(jsonPath("$.status").value(true))
+					.andReturn();
+					
+		
+	}
+	
+	/**
+	 * Function to test register account throws exception
+	 * @throws Exception
+	 */
+	@DisplayName("Test register account that throws exception")
+	@Test
+	public void testRegisterAccountThrowsException() throws Exception{
+		String error  = this.mockMvc.perform(post("/register").contentType("application/json").content(new ObjectMapper().writeValueAsString(this.user)))
+									.andDo(print())
+									.andExpect(status().isBadRequest())	
+									.andReturn().getResolvedException().getMessage();
+		
+		assertEquals("Email Id exists.Cannot create duplicate entity.",error);
+	}
+	
+	/**
+	 * Function to test account activation
+	 * @throws Exception
+	 */
+	@DisplayName("Testing account activation")
+	@Test
+	public void testActivateAccount() throws Exception{	
+		String token = "8b74d644e6a24e0ebe939bdbf396a040";
+		this.mockMvc.perform(get("/activate").contentType("application/json").param("token", token))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("$.message").value("Your account has been activated.Please login with your credentials"))
+					.andExpect(jsonPath("$.status").value(true))
+					.andReturn();
+	}
+	
+	/**
+	 * Function to test account activation by supplying invalid toeken.
+	 * @throws Exception
+	 */
+	@DisplayName("Testing account activation by supplying invalid token")
+	@Test
+	public void testActivateAccountReturnsFails() throws Exception{
+		String token = "8b74d644e6a24e0ebe939bdbf396a041";
+		this.mockMvc.perform(get("/activate").contentType("application/json").param("token", token))
+					.andDo(print())
+					.andExpect(status().isUnauthorized())
+					.andExpect(jsonPath("$.message").value("Invalid token.Please provide valid token"))
+					.andExpect(jsonPath("$.status").value(false))
+					.andReturn();
+	}
+
+}
