92d79178ed2c75041aefb92b03a263a49653bf0b
unknown <swathyphpfreelancer@gmail.com>
2019-08-01 14:45:02 -0400

    added redis config

--- src/main/java/com/springboot/genericauthentication/GenericAuthenticationApplication.java
+++ src/main/java/com/springboot/genericauthentication/GenericAuthenticationApplication.java
@@ -3,7 +3,11 @@ package com.springboot.genericauthentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.springboot.genericauthentication.models.JwtToken;
 
 @SpringBootApplication
 public class GenericAuthenticationApplication {
@@ -22,6 +26,19 @@ public class GenericAuthenticationApplication {
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}	 
+	
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		return new JedisConnectionFactory();
+	}
+	
+	
+	@Bean
+	  RedisTemplate<String, JwtToken> redisTemplate(){
+	    RedisTemplate<String,JwtToken> redisTemplate = new RedisTemplate<String, JwtToken>();
+	    redisTemplate.setConnectionFactory(jedisConnectionFactory());
+	    return redisTemplate;
+	  }
  
 	 
 }
